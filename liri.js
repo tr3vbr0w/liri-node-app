b8936c591b363ce9536459e6ae2e0a0fb6016d54
tbsteeze <trevbrow@live.com>
2019-09-12 22:52:16 -0600

    functioning bands in town, need to get spotify and OMDB apis working

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+var axios = require("axios");
+// require("dotenv").config();
 
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+
+// var spotify = new Spotify(keys.spotify);
 //Make liri.js so that it can take in one of the following commands
 // * `concert-this`
 
@@ -11,3 +13,27 @@ var spotify = new Spotify(keys.spotify);
 // * `movie-this`
 
 // * `do-what-it-says`
+//Set two variables to take the user input from the command line. After the address and npm package, process.argv[2] represents the case in the switch statement, process.argv[3] represents the band, song or movie the user requests information about
+
+var command = process.argv[2];
+var media = process.argv[3];
+//This function is called when the user enters 'concert-this' into the command variable
+function bandsInTownReq() {
+    var artist = media;
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function(r) {
+            console.log(r.data[1].venue)
+        }
+    )
+
+}
+//This function determines what the user is interested in returning. I.E movie, song, or concert sought
+function detectSelector() {
+    switch(command) {
+        case 'concert-this':
+            bandsInTownReq();
+            break;
+    }
+}
+
+detectSelector();
